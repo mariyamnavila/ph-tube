77a384fdadcd3fbd6bb7655a9b95e74f26f24a66
mariyamnavila <bibimariyamnavila@gmail.com>
2025-07-24 21:15:12 +0600

    added categories and videos

---
+++ scripts.js/test.js
@@ -0,0 +1,10 @@
+const isVerified = true;
+// if (isVerified) {
+//     console.log('kkk');
+
+// } else {
+//     console.log('lll');
+
+// }
+
+console.log(`${isVerified === true ? 'lala' : 'kakaka' }`);
